8ab298eb5f11642333c69b3a7ec3ead9e9c6a394
ayako1967 <takuya_kctn1414@tbz.t-com.ne.jp>
2016-10-23 20:06:20 +0900

    第2段階

--- app/src/main/java/jp/techacademy/takuya/okitsu/aisatsuapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/takuya/okitsu/aisatsuapp/MainActivity.java
@@ -1,7 +1,16 @@
 package jp.techacademy.takuya.okitsu.aisatsuapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
 
