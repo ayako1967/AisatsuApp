4e67163a1feaaf4c17af00f540be5216f83f0ea3
ayako1967 <takuya_kctn1414@tbz.t-com.ne.jp>
2016-10-23 21:18:14 +0900

    onCreate メソッド作成 (エラー発生)

--- app/src/main/java/jp/techacademy/takuya/okitsu/aisatsuapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/takuya/okitsu/aisatsuapp/MainActivity.java
@@ -20,5 +20,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+
+        mTextView = (TextView) findViewById(R.id.textView);
     }
+
 }
